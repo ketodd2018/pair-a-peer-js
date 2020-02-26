49fa0daf7a4ff4c05d45a2a48e0626feb1358ad6
Karen Todd <ketodd2018@gmail.com>
2020-02-26 11:34:29 -0600

    Started work on peer mentors

--- script.js
+++ script.js
@@ -2,11 +2,34 @@ window.addEventListener("load", function(){
     const completeForm = document.getElementById("studentInput");
     let name = document.querySelector("input[name=name]");
     let age = document.querySelector("input[name=age]");
+    let degree = document.querySelector("input[name=degree]");
+    let progress = document.querySelector("input[name=progress]");
+    // let peerMentor = document.getElementById("peerMentor");
+    
 
     completeForm.addEventListener("submit", function(event){
+       
         if (name.value==="" || age.value===""){
             alert("One or more fields are missing required entry.");
             event.preventDefault();
         };
+        // if (name.value!=="" && age.value!==""){
+        //    degree.style.visibility = "visible";
+        //    event.preventDefault();
+        // };
+        // switch(degree.value){
+        //     case "Associate in Arts":
+        //         if (age <= 22){
+        //             if (progress.value === "First Year"){
+        //                 document.getElementById("peerMentor").innerHTML="Your Peer Mentor is: Preston Carver\nSpecialty: First Year Associate in Arts students";
+        //                 event.preventDefault();
+        //             }
+        //         break;
+        //         }
+        //     default:
+        //         alert("One or more fields are missing required entry.");
+        //         event.preventDefault();
+        //         break;        
+        // }
     });
 });
