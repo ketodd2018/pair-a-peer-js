e9a6a226e8d051cd4fbba375c0e02d96b185df8a
Karen Todd <ketodd2018@gmail.com>
2020-03-12 17:25:28 -0500

    Ask for help

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
 window.addEventListener("load", function(){
     const completeForm = document.getElementById("studentInput");
-    let name = document.querySelector("input[name=name]");
-    let age = document.querySelector("input[name=age]");
-    let degree = document.querySelector("input[name=degree]");
-    let progress = document.querySelector("input[name=progress]");
-    // let peerMentor = document.getElementById("peerMentor");
+    const name = document.querySelector("input[name=name]");
+    const age = document.querySelector("input[name=age]");
+    const peerMentor = document.getElementById("peerMentor");
+    const button = document.getElementById("submitButton");
     
 
     completeForm.addEventListener("submit", function(event){
-       
-        if (name.value==="" || age.value===""){
-            alert("One or more fields are missing required entry.");
-            event.preventDefault();
-        };
-        // if (name.value!=="" && age.value!==""){
-        //    degree.style.visibility = "visible";
-        //    event.preventDefault();
-        // };
-        // switch(degree.value){
-        //     case "Associate in Arts":
-        //         if (age <= 22){
-        //             if (progress.value === "First Year"){
-        //                 document.getElementById("peerMentor").innerHTML="Your Peer Mentor is: Preston Carver\nSpecialty: First Year Associate in Arts students";
-        //                 event.preventDefault();
-        //             }
-        //         break;
-        //         }
-        //     default:
-        //         alert("One or more fields are missing required entry.");
-        //         event.preventDefault();
-        //         break;        
-        // }
+        button.onclick = ()=> {
+            const degree = document.getElementById("degree").value;
+            const progress = document.getElementById("progress").value;
+
+            
+            if (name.value==="" || age.value===""){
+                alert("One or more fields are missing required entry.");
+                event.preventDefault();
+            };
+            // if (name.value!=="" && age.value!==""){
+            //    peerMentor.style.visibility = "visible";
+            //    event.preventDefault();
+            // };
+            if(degree === "arts"){
+                // if (age <= 22){
+                //     if (progress === "firstYear"){
+                //         peerMentor.innerHTML=`
+                //         <p>Your Peer Mentor is: Preston Carver\nSpecialty: First Year Associate in Arts students</p>
+                //         `;
+                //         event.preventDefault();
+                //     }else if (progress === "secondYear"){
+                //         peerMentor.innerHTML=`
+                //         <p>Your Peer Mentor is: _____</p>
+                //         `;
+                //     }
+                // };    
+                alert("IT WORKS");
+                event.preventDefault();
+            }else{
+                alert("DOESNT WORK");
+                event.preventDefault();
+            };
+    };
     });
 });
