ba67b5b4372efc4b76d8ab6cb4f92e6a04d3ff6d
Karen Todd <ketodd2018@gmail.com>
2020-05-11 19:50:59 -0500

    Peer Mentor works

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 window.addEventListener("load", function(){
     const completeForm = document.getElementById("studentInput");
-    const name = document.querySelector("input[name=name]");
-    const age = document.querySelector("input[name=age]");
-    const peerMentor = document.getElementById("peerMentor");
     const button = document.getElementById("submitButton");
-    
+
 
     completeForm.addEventListener("submit", function(event){
-        // button.onclick = ()=> {
             const degree = document.getElementById("degree").value;
             const progress = document.getElementById("progress").value;
-
+            const name = document.querySelector("input[name=name]");
+            const age = document.querySelector("input[name=age]");
+            const peerMentor = document.getElementById("peerMentor");
             
             if (name.value==="" || age.value===""){
                 alert("One or more fields are missing required entry.");
                 event.preventDefault();
             };
-            // if (name.value!=="" && age.value!==""){
-            //    peerMentor.style.visibility = "visible";
-            //    event.preventDefault();
-            // };
+            if (name.value!=="" && age.value!==""){
+                peerMentor.style.visibility = "visible";
+                event.preventDefault();
+                };
             if(degree === "arts"){
                 // if (age <= 22){
-                //     if (progress === "firstYear"){
-                //         peerMentor.innerHTML=`
-                //         <p>Your Peer Mentor is: Preston Carver\nSpecialty: First Year Associate in Arts students</p>
-                //         `;
-                //         event.preventDefault();
-                //     }else if (progress === "secondYear"){
-                //         peerMentor.innerHTML=`
-                //         <p>Your Peer Mentor is: _____</p>
-                //         `;
-                //     }
+                    if (progress === "firstYear"){
+                        peerMentor.innerHTML=`
+                        Your Peer Mentor is: Preston Carver - 
+                        Specialty: First Year Associate in Arts students
+                        `;
+                        event.preventDefault();
+                    }else if (progress === "secondYear"){
+                        peerMentor.innerHTML=`
+                        <p>Your Peer Mentor is: _____</p>
+                        `;
+                    }
                 // };    
                 alert("IT WORKS");
                 event.preventDefault();
@@ -39,6 +38,5 @@ window.addEventListener("load", function(){
                 alert("DOESNT WORK");
                 event.preventDefault();
             };
-        // }
     });
 });
