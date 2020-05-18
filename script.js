ff6da7e171b4ae68a1c88a4f340d352350c35b59
Karen Todd <ketodd2018@gmail.com>
2020-05-18 12:40:32 -0500

    Removed age - finished project

--- script.js
+++ script.js
@@ -10,32 +10,71 @@ window.addEventListener("load", function(){
             const age = document.querySelector("input[name=age]");
             const peerMentor = document.getElementById("peerMentor");
             
-            if (name.value==="" || age.value===""){
+            if (name.value==="" || progress==="" || degree===""){
                 alert("One or more fields are missing required entry.");
                 event.preventDefault();
             };
-            if (name.value!=="" && age.value!==""){
+            if (name.value!=="" && progress!=="" && degree!==""){
                 peerMentor.style.visibility = "visible";
                 event.preventDefault();
                 };
             if(degree === "arts"){
-                // if (age <= 22){
-                    if (progress === "firstYear"){
-                        peerMentor.innerHTML=`
-                        Your Peer Mentor is: Preston Carver - 
-                        Specialty: First Year Associate in Arts students
-                        `;
-                        event.preventDefault();
-                    }else if (progress === "secondYear"){
-                        peerMentor.innerHTML=`
-                        <p>Your Peer Mentor is: _____</p>
-                        `;
-                    }
-                // };    
-                alert("IT WORKS");
-                event.preventDefault();
+                if (progress === "firstYear"){
+                    peerMentor.innerHTML=`
+                    Your Peer Mentor is: Preston Carver - Specialty: First Year Associate in Arts students
+                    `;
+                    event.preventDefault();
+                }else if (progress === "secondYear"){
+                    peerMentor.innerHTML=`
+                    Your Peer Mentor is: Ryan Nelson - Specialty: Second Year Associate in Arts students
+                    `;
+                    event.preventDefault();
+                }else if (progress === "returning"){
+                    peerMentor.innerHTML=`
+                    Your Peer Mentor is: Skylar Lee - Specialty: Returning Associate in Arts students
+                    `;
+                    event.preventDefault();
+                };
+            }else if(degree === "science"){
+                if (progress === "firstYear"){
+                    peerMentor.innerHTML=`
+                    Your Peer Mentor is: Liam Oliver - Specialty: First Year Associate in Science students
+                    `;
+                    event.preventDefault();
+                }else if (progress === "secondYear"){
+                    peerMentor.innerHTML=`
+                    Your Peer Mentor is: Alex Trotter - Specialty: Second Year Associate in Science students
+                    `;
+                    event.preventDefault();
+                }else if (progress === "returning"){
+                    peerMentor.innerHTML=`
+                    Your Peer Mentor is: Jeremy Linton - Specialty: Returning Associate in Science students
+                    `;
+                    event.preventDefault();
+                };            
+            }else if(degree === "cert"){
+                if (progress === "firstYear"){
+                    peerMentor.innerHTML=`
+                    Your Peer Mentor is: Michael Scott - Specialty: First Year Certification Program students
+                    `;
+                    event.preventDefault();
+                }else if (progress === "secondYear"){
+                    peerMentor.innerHTML=`
+                    Your Peer Mentor is: Baron James - Specialty: Second Year Certification Program students
+                    `;
+                    event.preventDefault();
+                }else if (progress === "returning"){
+                    peerMentor.innerHTML=`
+                    Your Peer Mentor is: Gina Castillo - Specialty: Returning Certification Program students
+                    `;
+                    event.preventDefault();
+                };            
+            }else if(degree === "noncredit"){
+                peerMentor.innerHTML=`
+                Your Peer Mentor is: Stephanie Kirkwood - Specialty: Non Credit students
+                `;
             }else{
-                alert("DOESNT WORK");
+                alert("Something went wrong. Please try again.");
                 event.preventDefault();
             };
     });
